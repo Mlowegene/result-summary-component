fd848d049a24f5a81f2e9f484601c7721fe15eae
Mlowegene <genemlowe@gmail.com>
2023-03-14 00:04:40 +0300

    take data from JSON file using js

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
-
 function fetchData() {
-    fetch('./results-summary-component-main/data.json')
+  fetch("./results-summary-component-main/data.json")
     .then((response) => response.json())
-    .then((data) => console.log(data));
-    
+    .then((data) => {
+      console.log(data);
+
+      const resultContainer = document.querySelector(".result-descriptions");
+
+      data.forEach((datas) => {
+        const result = document.createElement("div");
+        result.className = `result-description ${datas.category
+          .toLowerCase()
+          .replace(/\s+/g, "-")}`;
+        result.innerHTML = `
+    <img class="reaction-image" src=${datas.icon} alt="reaction-icon" />
+    <p class="title">${datas.category}</p>
+    <p class="value"><span class="number-color">${datas.score}</span> <span class="default-color">/ 100</span></p>
+  `;
+        resultContainer.appendChild(result);
+      });
+    });
 }
 
 fetchData();
